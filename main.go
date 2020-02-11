c13a6216401a67f8f3c79cc48905f2edb3cff3be
kineticmarginal <43065450+kineticmarginal@users.noreply.github.com>
2020-02-11 10:47:40 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -6,27 +6,59 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
+	"sort"
 )
 
 var (
 	d = flag.String("d", ".", "Directory to process")
 	a = flag.Bool("a", false, "Print all info")
+	h = flag.Bool("h", false, "Print size in human-readable format ")
+	sorted = flag.String("sort", "", "Sort files in directory")
 )
 
 func hrSize(fsize int64) string {
-	return "0KB"
+	if (fsize < 1024) {
+		return strconv.Itoa(int(fsize)) + "B"
+	} else if (fsize >= 1024 && fsize < 1024*1024){
+		fsize /= 1024
+		return strconv.Itoa(int(fsize)) + "KB"
+	} else if (fsize >= 1024*1024 && fsize < 1024*1024*1024){
+		fsize /= 1024*1024
+		return strconv.Itoa(int(fsize)) + "MB"
+	}
+	return "0B"
 }
 
 func printAll(file os.FileInfo) {
 	time := file.ModTime().Format("Jan 06 15:4")
 	fSize := strconv.Itoa(int(file.Size()))
-	fmt.Printf("%s %s %s \n", fSize, time, file.Name())
+	if *h {
+		fmt.Printf("%s %s %s \n", hrSize(int64(file.Size())), time, file.Name())
+	} else {
+		fmt.Printf("%s %s %s \n", fSize, time, file.Name())
+	}
+}
+
+type SortByDate []os.FileInfo
+
+func (ss SortByDate) Len() int{
+	return len(ss)
+}
+
+func (ss SortByDate) Swap(i int, j int) {
+	ss[i], ss[j] = ss[j], ss[i]
+}
+
+func (ss SortByDate) Less(i int, j int) bool {
+	return ss[i].ModTime().UnixNano() < ss[j].ModTime().UnixNano()
 }
 
 func main() {
 	flag.Parse()
 	files, _ := ioutil.ReadDir(*d)
-
+	if *sorted == "date" {
+		sort.Sort(SortByDate(files))
+	}
 	for _, file := range files {
 		if *a {
 			printAll(file)
